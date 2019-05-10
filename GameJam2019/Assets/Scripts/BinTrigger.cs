907bb5fccd0feb1298f6591987e904f5e306bcf5
Taylor Fowler <Taylor-Fowler@users.noreply.github.com>
2019-05-10 20:00:51 +0100

    Added Escalator prefab and bin trigger
    Bin trigger detects when the rubbish collides with the triggerable collection area of the bin

---
+++ GameJam2019/Assets/Scripts/BinTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BinTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Bin bin;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RubbishMovement rm = other.gameObject.GetComponent<RubbishMovement>();
+        if (rm != null)
+        {
+            bin.PickupRubbish(rm.gameObject);
+        }
+    }
+}
