832c238dc79bfd64a49120afe8c5884e68191386
Taylor Fowler <Taylor-Fowler@users.noreply.github.com>
2019-05-10 20:23:13 +0100

    Updated Bin animation

--- GameJam2019/Assets/Scripts/Bin.cs
+++ GameJam2019/Assets/Scripts/Bin.cs
@@ -8,24 +8,55 @@ public class Bin : MonoBehaviour
     [SerializeField]
     private MeshRenderer circularOutlineMeshRenderer;
 
+    [SerializeField]
+    private Color collectionOutlineColor;
+
+    [SerializeField]
+    private float colorAnimationTime = 1.0f;
+
+    [SerializeField]
+    private AnimationCurve animationCurve;
+    
+    private Material outlineMaterial;
+    private float timePerAnimation;
+
+    #region Unity API
+    private void Awake()
+    {
+        outlineMaterial = circularOutlineMeshRenderer.material;
+        timePerAnimation = colorAnimationTime * 0.5f;
+    }
+    #endregion
 
     public void PickupRubbish(GameObject pickedUpObject)
     {
         Destroy(pickedUpObject);
+        StartCoroutine(AnimateOutline());
         // Trigger score
         Debug.Log("Bin just collected some rubbish!");
     }
 
     private IEnumerator AnimateOutline()
     {
-        const float totalTime = 3.0f;
+        float timeElapsed = 0.0f;
+        float curveSample = 0.0f;
+        Color initialColor = outlineMaterial.color;
+
+        while (timeElapsed < colorAnimationTime)
+        {
+            curveSample = animationCurve.Evaluate(timeElapsed / timePerAnimation);
+            outlineMaterial.color = Color.Lerp(initialColor, collectionOutlineColor, curveSample);
+
+            yield return null;
+            timeElapsed += Time.deltaTime;
+        }
+    }
+
+    private IEnumerator AnimatePickup()
+    {
         float timeElapsed = 0.0f;
 
-        while(timeElapsed < totalTime)
-        {
-            yield return null;
-        }
-        
+        yield return null;
     }
 
 }
