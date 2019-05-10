3c095ce48c91eb9ceca68d5f26a28f7bf7cce560
Taylor Fowler <Taylor-Fowler@users.noreply.github.com>
2019-05-10 23:18:27 +0100

    Began pickup system

---
+++ GameJam2019/Assets/Scripts/Pickups/AbstractPickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public abstract class AbstractPickup : ScriptableObject
+{
+    public float EffectTime { get { return effectTime; } }
+
+    [SerializeField]
+    private float effectTime;
+
+    public abstract void Activate(CarController carController);
+    public abstract void Deactivate(CarController carController);
+}
