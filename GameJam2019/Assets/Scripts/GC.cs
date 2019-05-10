8c7e996532740cdbc4e3f1dcb4de937693ad1af9
Max Bryans <maxsfw3@gmail.com>
2019-05-10 23:15:54 +0100

    GUI and FLashes operational

--- GameJam2019/Assets/Scripts/GC.cs
+++ GameJam2019/Assets/Scripts/GC.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GC : MonoBehaviour
 {
 
     #region MEMBERS
+    public FlashProManager FM;
+    private TextMeshProUGUI GUItext1;
     #endregion
 
     #region Set Singelton
@@ -38,12 +41,15 @@ public class GC : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(gameObject);
+
+        // As the GC is a pre-fab ... best make it a "hacky - find"
+        if (FM == null) FM = FindObjectOfType<FlashProManager>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpGUI();
     }
 
     // Update is called once per frame
@@ -53,4 +59,12 @@ public class GC : MonoBehaviour
     }
 
     #endregion
+
+    void SetUpGUI()
+    {
+        GameObject GUIItem1 = FM.AddGUIItem("Score : 0 - 0", 0.4f, 0.95f, 0.2f, Color.white);
+        GUItext1 = GUIItem1.GetComponent<TextMeshProUGUI>();
+
+    }
+
 }
