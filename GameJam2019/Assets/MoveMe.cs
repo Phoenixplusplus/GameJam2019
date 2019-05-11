bad99d50b59fb95508e5e0ed9c16384031b74afe
MaxBryans <43577921+MaxBryans@users.noreply.github.com>
2019-05-11 12:50:23 +0100

    FINAL escalators .. not to get teh car doing it

---
+++ GameJam2019/Assets/MoveMe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour, IMoveable<Vector3>
+{
+    private Rigidbody RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    public void MoveBy(Vector3 disp)
+    {
+        RB.isKinematic = true;
+        transform.Translate(disp);
+        RB.isKinematic = false;
+
+    }
+
+}
+
---
+++ GameJam2019/Assets/Step_Control.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step_Control : MonoBehaviour
+{
+    public float cutoff = 1f;
+    public float ForceMultiplier = 1;
+    [SerializeField]
+    private Vector3 MyDisp;
+    [SerializeField]
+    private Vector3 OldPos;
+
+    void Start ()
+    {
+        MyDisp = Vector3.zero;
+        OldPos = transform.position;
+    }
+
+    void Update()
+    {
+        MyDisp = transform.position - OldPos;
+        if (MyDisp.sqrMagnitude > cutoff)
+        {
+            MyDisp = Vector3.zero;
+        }
+        OldPos = transform.position;
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //IMoveable<Vector3> thing = collision as IMoveable<Vector3>;
+
+        IMoveable<Vector3> thing = collision.gameObject.GetComponent<IMoveable<Vector3>>();
+
+        if (thing != null)
+        {
+            thing.MoveBy(MyDisp * ForceMultiplier);
+        }
+        //Debug.Log("Still Here");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IMoveable<Vector3> thing = collision.gameObject.GetComponent<IMoveable<Vector3>>();
+
+        if (thing != null)
+        {
+            thing.MoveBy(MyDisp * ForceMultiplier);
+        }
+        //Debug.Log("Boing");
+    }
+
+
+
+
+
+}
