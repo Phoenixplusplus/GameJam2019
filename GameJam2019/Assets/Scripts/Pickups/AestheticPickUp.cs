ae3a94a27ede5d607c4d710a16469ce1a3336213
Phoenixplusplus <phoenixmee91@gmail.com>
2019-05-11 20:52:04 +0900

    Mall scene

--- GameJam2019/Assets/Scripts/Pickups/AestheticPickUp.cs
+++ GameJam2019/Assets/Scripts/Pickups/AestheticPickUp.cs
@@ -13,7 +13,7 @@ public class AestheticPickUp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
